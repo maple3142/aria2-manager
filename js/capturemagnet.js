cb101487d9344da7ffe3e62a4c1bfcc4aef7be78
maple3142 <kirby741852963@gmail.com>
2019-02-07 18:30:55 +0800

    update capturemagnet

--- js/capturemagnet.js
+++ js/capturemagnet.js
@@ -1,45 +1,13 @@
-const storage = {
-	get: (key, defaultval) =>
-		new Promise(res => {
-			chrome.storage.sync.get([key], result => {
-				res(typeof result[key] === 'undefined' ? defaultval : result[key])
-			})
-		}),
-	set: (key, val) =>
-		new Promise(res => {
-			chrome.storage.sync.set({ [key]: val }, res)
-		}),
-	getMult: obj =>
-		new Promise(res => {
-			const keys = Object.keys(obj)
-			chrome.storage.sync.get(keys, result => {
-				for (const [k, v] of Object.entries(obj)) {
-					if (typeof result[k] === 'undefined') {
-						result[k] = v
-					}
-				}
-				res(result)
-			})
-		}),
-	setMult: obj =>
-		new Promise(res => {
-			chrome.storage.sync.set(obj, res)
+document.addEventListener('click', async e => {
+	const {
+		target: { tagName, href }
+	} = e
+	if (tagName === 'A' && href.startsWith('magnet:')) {
+		e.preventDefault()
+		e.stopPropagation()
+		chrome.runtime.sendMessage({
+			action: 'magnetCaptured',
+			data: href
 		})
-}
-storage.get('captureMagnet').then(cap=>{
-	if(!cap)return
-	document.addEventListener('click', async e => {
-		const {
-			target: { tagName, href }
-		} = e
-		if (tagName === 'A' && href.startsWith('magnet:')) {
-			e.preventDefault()
-			e.stopPropagation()
-			chrome.runtime.sendMessage({
-				action: 'magnetCaptured',
-				data: href
-			})
-		}
-	})
-	
+	}
 })
